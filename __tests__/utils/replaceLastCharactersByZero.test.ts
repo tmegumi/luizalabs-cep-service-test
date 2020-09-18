816326dcbd52462b0aa8c344ca0b8f694bbff6f1
Thais Megumi <tha.megumi@gmail.com>
2020-09-17 21:51:39 -0300

    Create replaceLastCharactersByZero function tests

---
+++ __tests__/utils/replaceLastCharactersByZero.test.ts
@@ -0,0 +1,23 @@
+import replaceLastCharactersByZero from '../../src/utils/replaceLastCharactersByZero';
+
+describe('Function replaceLastCharactersByZero', () => {
+  it('should not replace any character', () => {
+    const result = replaceLastCharactersByZero('Some text to test', 0);
+
+    expect(result).toBe('Some text to test');
+  });
+
+  it("should replace last character as '0'", () => {
+    const result = replaceLastCharactersByZero('Some text to test', 1);
+
+    expect(result).toBe('Some text to tes0');
+  });
+
+  it("should replace all characters as '0'", () => {
+    const text = 'Some text to test';
+
+    const result = replaceLastCharactersByZero(text, text.length);
+
+    expect(result).toBe('00000000000000000');
+  });
+});
